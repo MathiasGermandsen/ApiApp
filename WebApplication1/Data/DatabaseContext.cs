e5636d03b05146c564aea7d1a750bb4b425e7f40
mathias <mathias.germandsen@bestseller.com>
2025-01-16 09:15:43 +0100

    test

--- WebApplication1/Data/DatabaseContext.cs
+++ WebApplication1/Data/DatabaseContext.cs
@@ -5,23 +5,19 @@ namespace WebApplication1.Data;
 
 public class DatabaseContext : DbContext
 {
-        
-        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
-        
-        public DbSet<Models.User> User { get; set; }
-        // public DbSet<Models.Room> Rooms { get; set; }
-        public DbSet<Models.Car> Car { get; set; }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Car> Cars { get; set; }
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-                // Configure the relationship: One User has many Cars
-                modelBuilder.Entity<User>()
-                        .HasMany(u => u.Cars)
-                        .WithOne(c => c.User)
-                        .HasForeignKey(c => c.UserId);
+    public DbSet<User> Users { get; set; }
+    public DbSet<Car> Cars { get; set; }
 
-                base.OnModelCreating(modelBuilder);
-        }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Configure the relationship: One User has many Cars
+        modelBuilder.Entity<User>()
+            .HasMany(u => u.Cars)
+            .WithOne(c => c.User)
+            .HasForeignKey(c => c.UserId);
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
