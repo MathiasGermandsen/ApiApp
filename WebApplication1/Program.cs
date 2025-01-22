5d00a7f6615e5dbb9151d37f01c0708f7c11403e
Mathias Germandsen <141312755+MathiasGermandsen@users.noreply.github.com>
2025-01-22 09:50:24 +0100

    Update Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Scaffolding.Metadata;
 using WebApplication1.Data;
 
 namespace WebApplication1
@@ -10,7 +11,8 @@ namespace WebApplication1
             var builder = WebApplication.CreateBuilder(args);
 
             var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-            
+
+            // Specify when frontend is created
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy(
@@ -18,27 +20,27 @@ namespace WebApplication1
                     policy => { policy.WithOrigins("*").AllowAnyMethod().AllowAnyHeader(); }
                 );
             });
-            
+
+            // Add services to the container.
+
             builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             IConfiguration Configuration = builder.Configuration;
-
             // Configure Entity Framework with PostgreSQL
             string connectionString = Configuration.GetConnectionString("DefaultConnection")
-                           ?? Environment.GetEnvironmentVariable("DefaultConnection");
+                                      ?? Environment.GetEnvironmentVariable("DefaultConnection");
 
             builder.Services.AddDbContext<DatabaseContext>(options =>
                 options.UseNpgsql(connectionString));
 
             var app = builder.Build();
 
-            // Apply migrations
-            ApplyMigrations(app);
-
             app.UseCors(MyAllowSpecificOrigins);
 
-            // Configure the HTTP request pipeline
+            // Configure the HTTP request pipeline.
+            // Swagger link: https://apiexampleproject.onrender.com/swagger/index.html
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
@@ -49,16 +51,22 @@ namespace WebApplication1
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
-            
+
+            //app.UseHttpsRedirection();
+
+            //app.UseAuthorization();
+
+
             app.MapControllers();
-            app.MapGet("/", async context =>
+            app.MapGet("/", async context => 
             {
                 context.Response.Redirect("/swagger");
                 await Task.CompletedTask;
             });
-
+            
             app.Run();
         }
+    }
         
         private static void ApplyMigrations(WebApplication app)
         {
